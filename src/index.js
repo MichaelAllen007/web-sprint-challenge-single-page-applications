d89db932619bb6ce54649dbdb2523df221b8e161
unknown <allenmk20073@yahoo.com>
2021-02-21 22:03:25 -0500

    MVP. Tests passing.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter as Router} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import "./index.css";
 import App from "./App";
-import {BrowserRouter as Router} from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.css'
-
 
 ReactDOM.render(
     <Router>
         <App />
-    </Router>, document.getElementById("root"));
+    </Router>,
+    document.getElementById("root")
+);
